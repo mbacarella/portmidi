35c2330268607ef2ef133789477b28233c5754f0
Michael Bacarella <michael.bacarella@gmail.com>
2021-07-08 10:31:58 -0700

    more functions

--- c/portmidi_c_function_descriptions.ml
+++ c/portmidi_c_function_descriptions.ml
@@ -8,4 +8,6 @@ module Functions (F : Ctypes.FOREIGN) = struct
   let pm_initialize = foreign "Pm_Initialize" (void @-> returning int)
   let pm_terminate = foreign "Pm_Terminate" (void @-> returning void)
   let pm_count_devices = foreign "Pm_CountDevices" (void @-> returning int)
+  let pm_get_device_info =
+    foreign "Pm_GetDeviceInfo" (int @-> returning (ptr Types.Portmidi_device_info.t))
 end
--- portmidi.ml
+++ portmidi.ml
@@ -1,5 +1,38 @@
+open! Base
+
+module Device_info = struct
+  type t =
+    { interface : string
+    ; name : string
+    ; input : bool
+    ; output : bool
+    ; struct_version_internal : int
+    ; opened_internal : bool }
+  [@@deriving sexp, fields]
+end
+
 module Constants = struct
   open C.Types
+
+  let result_of_int i =
+    let open Pm_error in
+    if Int.(=) i no_error then Ok ()
+    else if Int.(=) i no_data then Ok ()
+    else if Int.(=) i got_data then Error `Got_data
+    else if Int.(=) i host_error then Error `Host_error
+    else if Int.(=) i invalid_device_id then Error `Invalid_device_id
+    else if Int.(=) i insufficient_memory then Error `Insufficient_memory
+    else if Int.(=) i buffer_too_small then Error `Buffer_too_small
+    else failwith (Printf.sprintf "unknown PmError code: %d" i)
+
+  let device_info_of_pdi pdi =
+    let module PDI = Portmidi_device_info in
+    { Device_info.struct_version_internal = pdi.PDI.struct_version
+    ; interface = pdi.PDI.interf
+    ; name = pdi.PDI.name
+    ; input = Int.(=) pdi.PDI.input 1
+    ; output = Int.(=) pdi.PDI.output 1
+    ; opened_internal = Int.(=) pdi.PDI.opened 1
 
   let default_sysex_buffer_size = default_sysex_buffer_size
 end
@@ -8,9 +41,13 @@ module Functions = struct
   (*open Ctypes*)
   open C.Functions
 
-  let initialize = pm_initialize
-  let terminate = pm_terminate
+  let initialize () =
+    Constants.result_of_int (pm_initialize ())
+  let terminate () = pm_terminate
   let count_devices = pm_count_devices
+  let get_device_info index =
+    let di = pm_get_device_info index in
+    Constnats.device_info_of_pdi di
 end
 
 include Functions
